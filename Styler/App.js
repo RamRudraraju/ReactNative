6323f6460975d05db658d542670c8f2c180b2ab5
Rama krishna Raju Rudraraju <rrkr2016@uab.edu>
2020-06-20 01:35:57 -0500

    Basic Styles:
    The main access to React Native screen is from Top to Bottom

---
+++ Styler/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+      return (
+        <View >
+           <View style={{ width:50,  height: 50,
+            backgroundColor:"#2475B0"}} /> 
+           <View style={{ width:100, height: 100,
+            backgroundColor:"#10A881"}} />     
+           <View style={{ width:150, height: 150,
+            backgroundColor:"#E74292"}} />
+        </View>
+      );
+  }
+}
+
+// style={styles.container}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
